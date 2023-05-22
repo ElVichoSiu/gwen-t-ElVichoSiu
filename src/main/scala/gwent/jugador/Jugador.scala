cfeef41b717dd6205463ab6686968b115ea82c16
ElVichoSiu <vthielemunoz@gmail.com>
2023-05-22 02:39:11 -0400

    Added features to Jugador

--- src/main/scala/gwent/jugador/Jugador.scala
+++ src/main/scala/gwent/jugador/Jugador.scala
@@ -3,6 +3,15 @@ package gwent.jugador
 
 import gwent.carta.ICarta
 
-class Jugador(val name: String, val deck: List[ICarta], var gems: Int = 2) extends IJugador {
+class Jugador(val name: String, private var _deck: List[ICarta], var gems: Int = 2) extends IJugador {
 
+  def deck: List[ICarta] = {
+    _deck
+
+  }
+
+  def shuffleDeck(): Unit = {
+    _deck = scala.util.Random.shuffle(_deck)
+
+  }
 }
