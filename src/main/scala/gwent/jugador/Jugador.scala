f972b69ccbf3a62a11ad004658dd2ff4d10ed72f
ElVichoSiu <vthielemunoz@gmail.com>
2023-05-22 20:08:35 -0400

    REFACTOR inClasses related to IJugador

--- src/main/scala/gwent/jugador/Jugador.scala
+++ src/main/scala/gwent/jugador/Jugador.scala
@@ -3,15 +3,22 @@ package gwent.jugador
 
 import gwent.carta.ICarta
 
-class Jugador(val name: String, private var _deck: List[ICarta], var gems: Int = 2) extends IJugador {
+class Jugador(val name: String, private var _deck: List[ICarta], var gems: Int = 2, private var _hand: List[ICarta] = List()) extends IJugador {
 
-  def deck: List[ICarta] = {
-    _deck
+  def deck: List[ICarta] = _deck
 
-  }
+  def hand: List[ICarta] = _hand
 
   def shuffleDeck(): Unit = {
     _deck = scala.util.Random.shuffle(_deck)
+
+  }
+
+  def drawCarta(): ICarta = {
+    val carta = deck.head
+    _deck = deck.tail
+    _hand = carta :: hand
+    carta
 
   }
 
--- src/test/scala/jugador/JugadorTest.scala
+++ src/test/scala/jugador/JugadorTest.scala
@@ -9,7 +9,7 @@ import munit.FunSuite
 class JugadorTest extends FunSuite {
   test("Jugador has a name, a deck and gems") {
     val cartadistancia = new ADistancia(name = "Arquero", description = "Unidad a distancia que posee 5 de strength", strength = 5)
-    val jugador = Jugador("Geralt", List(cartadistancia), 2)
+    val jugador = Jugador("Geralt", List(cartadistancia))
     assertEquals(jugador.name, "Geralt")
     assertEquals(jugador.deck, List(cartadistancia))
     assertEquals(jugador.gems, 2)
