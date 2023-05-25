f3ab54f5884c016da75c55e123e7745b9bd1a9bd
ElVichoSiu <vthielemunoz@gmail.com>
2023-05-24 22:52:46 -0400

    FEAT: Add new parameters in class constructor of Jugador

--- src/main/scala/gwent/jugador/Jugador.scala
+++ src/main/scala/gwent/jugador/Jugador.scala
@@ -3,7 +3,11 @@ package gwent.jugador
 
 import gwent.carta.ICarta
 
-class Jugador(val name: String, private var _deck: List[ICarta], var gems: Int = 2, private var _hand: List[ICarta] = List()) extends IJugador {
+import cl.uchile.dcc.gwent.carta.cartaunidad.classesunidad.{ADistancia, Asedio, CuerpoaCuerpo}
+
+class Jugador(val name: String, private var _deck: List[ICarta], var gems: Int = 2, 
+              private var _hand: List[ICarta] = List(), private var _caczone: List[CuerpoaCuerpo] = List(),
+              private var _asediozone: List[Asedio] = List(), private var _adistanciazone: List[ADistancia] = List()) extends IJugador {
 
   def deck: List[ICarta] = _deck
 
