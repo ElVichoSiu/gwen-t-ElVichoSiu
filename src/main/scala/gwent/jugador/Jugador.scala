86d62d504d2a7fd0f5a29af674cc81ebc6096bf2
ElVichoSiu <vthielemunoz@gmail.com>
2023-05-24 23:38:02 -0400

    FEAT: New functions added to Class Jugador

--- src/main/scala/gwent/jugador/Jugador.scala
+++ src/main/scala/gwent/jugador/Jugador.scala
@@ -5,9 +5,36 @@ import gwent.carta.ICarta
 
 import cl.uchile.dcc.gwent.carta.cartaunidad.classesunidad.{ADistancia, Asedio, CuerpoaCuerpo}
 
-class Jugador(val name: String, private var _deck: List[ICarta], var gems: Int = 2, 
+class Jugador(val name: String, private var _deck: List[ICarta], var gems: Int = 2,
               private var _hand: List[ICarta] = List(), private var _caczone: List[CuerpoaCuerpo] = List(),
               private var _asediozone: List[Asedio] = List(), private var _adistanciazone: List[ADistancia] = List()) extends IJugador {
+
+  def caczone: List[CuerpoaCuerpo] = _caczone
+
+  def adistanciazone: List[ADistancia] = _adistanciazone
+
+  def asediozone: List[Asedio] = _asediozone
+
+  def playCuerpoaCuerpo(c: CuerpoaCuerpo): Unit = {
+    if (hand.contains(c)) {
+      _hand = hand.filterNot(_ == c)
+      _caczone = c :: caczone
+    }
+  }
+
+  def playADistancia(c: ADistancia): Unit = {
+    if (hand.contains(c)) {
+      _hand = hand.filterNot(_ == c)
+      _adistanciazone = c :: adistanciazone
+    }
+  }
+
+  def playAsedio(c: Asedio): Unit = {
+    if (hand.contains(c)) {
+      _hand = hand.filterNot(_ == c)
+      _asediozone = c :: asediozone
+    }
+  }
 
   def deck: List[ICarta] = _deck
 
