b86e62d9e333839c0406745484138b3af9f0790e
ElVichoSiu <vthielemunoz@gmail.com>
2023-06-02 17:12:30 -0400

    TEST: Added test in Class JugadorTest for the methods shuffleDeck and drawCarta of the Class Jugador

--- src/test/scala/jugador/JugadorTest.scala
+++ src/test/scala/jugador/JugadorTest.scala
@@ -8,13 +8,16 @@ import cl.uchile.dcc.gwent.jugador.Jugador
 import munit.FunSuite
 
 class JugadorTest extends FunSuite {
-  val name = "Geralt"
-  val listvacia = List()
   var cartadistancia: ADistancia = _
   var cartacuerpoacuerpo: CuerpoaCuerpo = _
   var cartasedio: Asedio = _
   var cartaclimadespejado: ClimaDespejado = _
   var cartalluviatorrencial: LluviaTorrencial = _
+  val name = "Geralt"
+  var handi: List[ICarta] = _
+  var adistanciazonei: List[ADistancia] = _
+  var asediozonei: List[Asedio] = _
+  var caczonei: List[CuerpoaCuerpo] = _
   var deck: List[ICarta] = _
   var jugador: Jugador = _
   override def beforeEach(context: BeforeEach): Unit = {
@@ -23,17 +26,40 @@ class JugadorTest extends FunSuite {
     cartasedio = new Asedio("Torre", "Unidad de asedio que posee 20 de strength", 20)
     cartaclimadespejado = new ClimaDespejado()
     cartalluviatorrencial = new LluviaTorrencial()
+    handi = List()
+    adistanciazonei = List()
+    asediozonei = List()
+    caczonei = List()
     deck = List(cartadistancia,cartacuerpoacuerpo,cartasedio,cartaclimadespejado,cartalluviatorrencial)
     jugador = new Jugador(name, deck)
   }
+
   test("Jugador can be created with a name, a deck, gems, a hand, a caczone, a adistanciazone, a asediozone") {
     assertEquals(jugador.name, name)
     assertEquals(jugador.deck, deck)
     assertEquals(jugador.gems, 2)
-    assertEquals(jugador.hand, listvacia)
-    assertEquals(jugador.caczone, listvacia)
-    assertEquals(jugador.adistanciazone, listvacia)
-    assertEquals(jugador.asediozone, listvacia)
+    assertEquals(jugador.hand, handi)
+    assertEquals(jugador.adistanciazone, adistanciazonei)
+    assertEquals(jugador.asediozone, asediozonei)
+    assertEquals(jugador.caczone, caczonei)
+  }
+
+  test("Jugador is able to shuffle the deck") {
+    assertEquals(jugador.deck, deck)
+    jugador.shuffleDeck()
+    assertNotEquals(jugador.deck, deck)
+  }
+
+  test("Jugador is able to draw a carta from the deck") {
+    assertEquals(jugador.hand, handi)
+    assertEquals(jugador.deck, deck)
+    jugador.drawCarta()
+    assertNotEquals (jugador.hand, handi)
+    assertNotEquals(jugador.deck, deck)
+  }
+
+  test("Jugador is able to play") {
+
   }
 
 }
