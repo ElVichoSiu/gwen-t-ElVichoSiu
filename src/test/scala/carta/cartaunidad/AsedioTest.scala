8e009a13b891f48a7b32981a5055f6b46ec69335
ElVichoSiu <vthielemunoz@gmail.com>
2023-06-02 22:53:08 -0400

    REFACTOR: Changed the implementation of the initialization of the test in Classes CuerpoaCuerpoTest, ADistanciaTest, AsedioTest

--- src/test/scala/carta/cartaunidad/AsedioTest.scala
+++ src/test/scala/carta/cartaunidad/AsedioTest.scala
@@ -1,14 +1,28 @@
 package cl.uchile.dcc
 package carta.cartaunidad
 
-import cl.uchile.dcc.gwent.carta.cartaunidad.classesunidad.Asedio
+import cl.uchile.dcc.gwent.carta.ICarta
+import cl.uchile.dcc.gwent.carta.cartaunidad.classesunidad.{ADistancia, Asedio, CuerpoaCuerpo}
+import cl.uchile.dcc.gwent.jugador.Jugador
 import munit.FunSuite
 
 class AsedioTest extends FunSuite {
-  test("Asedio has a name and strength") {
-    val asedio = new Asedio("Torre", "Unidad de asedio que posee 20 de strength", 20)
-    assertEquals(asedio.name, "Torre")
-    assertEquals(asedio.strength, 20)
-  }
+  var cartadistancia: ADistancia = _
+  var cartasedio: Asedio = _
+  var cartacuerpoacuerpo: CuerpoaCuerpo = _
+  val name = "Geralt"
+  var handi: List[ICarta] = _
+  var asediozonei: List[Asedio] = _
+  var decki: List[ICarta] = _
+  var jugador: Jugador = _
 
+  override def beforeEach(context: BeforeEach): Unit = {
+    cartadistancia = new ADistancia("Arquero", "Unidad a distancia que posee 5 de strength", 5)
+    cartasedio = new Asedio("Torre", "Unidad de asedio que posee 20 de strength", 20)
+    cartacuerpoacuerpo = new CuerpoaCuerpo("Ghoul", "Unidad de combate cuerpo a cuerpo que posee 10 de strength", 10)
+    handi = List()
+    asediozonei = List()
+    decki = List(cartadistancia, cartacuerpoacuerpo, cartasedio)
+    jugador = new Jugador(name, decki)
+  }
 }
--- src/test/scala/carta/cartaunidad/CuerpoaCuerpoTest.scala
+++ src/test/scala/carta/cartaunidad/CuerpoaCuerpoTest.scala
@@ -1,14 +1,29 @@
 package cl.uchile.dcc
 package carta.cartaunidad
 
-import cl.uchile.dcc.gwent.carta.cartaunidad.classesunidad.CuerpoaCuerpo
+import cl.uchile.dcc.gwent.carta.ICarta
+import cl.uchile.dcc.gwent.carta.cartaunidad.classesunidad.{ADistancia, Asedio, CuerpoaCuerpo}
+import cl.uchile.dcc.gwent.jugador.Jugador
 import munit.FunSuite
 
 class CuerpoaCuerpoTest extends FunSuite {
-  test("CuerpoaCuerpo has a name and strength") {
-    val cuerpoacuerpo = new CuerpoaCuerpo("Ghoul", "Unidad de combate cuerpo a cuerpo que posee 10 de strength", 10)
-    assertEquals(cuerpoacuerpo.name, "Ghoul")
-    assertEquals(cuerpoacuerpo.strength, 10)
+  var cartadistancia: ADistancia = _
+  var cartasedio: Asedio = _
+  var cartacuerpoacuerpo: CuerpoaCuerpo = _
+  val name = "Geralt"
+  var handi: List[ICarta] = _
+  var caczonei: List[CuerpoaCuerpo] = _
+  var decki: List[ICarta] = _
+  var jugador: Jugador = _
+
+  override def beforeEach(context: BeforeEach): Unit = {
+    cartadistancia = new ADistancia("Arquero", "Unidad a distancia que posee 5 de strength", 5)
+    cartasedio = new Asedio("Torre", "Unidad de asedio que posee 20 de strength", 20)
+    cartacuerpoacuerpo = new CuerpoaCuerpo("Ghoul", "Unidad de combate cuerpo a cuerpo que posee 10 de strength", 10)
+    handi = List()
+    caczonei = List()
+    decki = List(cartadistancia, cartacuerpoacuerpo, cartasedio)
+    jugador = new Jugador(name, decki)
   }
 
 }
