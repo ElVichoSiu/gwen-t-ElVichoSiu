099567cbe33d113f3ced51e7fc5e756ce397f071
ElVichoSiu <vthielemunoz@gmail.com>
2023-06-02 21:39:02 -0400

    FEAT: Added methods in Trait IJugador

--- src/main/scala/gwent/jugador/IJugador.scala
+++ src/main/scala/gwent/jugador/IJugador.scala
@@ -1,6 +1,18 @@
 package cl.uchile.dcc
 package gwent.jugador
 
+import gwent.carta.ICarta
+import cl.uchile.dcc.gwent.carta.cartaunidad.classesunidad.{ADistancia, Asedio, CuerpoaCuerpo}
+
 trait IJugador {
+  def shuffleDeck(): Unit
+  
+  def drawCarta(): ICarta
+  
+  def playADistancia(c: ADistancia): Unit
+  
+  def playAsedio(c: Asedio): Unit
+  
+  def playCuerpoaCuerpo(c: CuerpoaCuerpo): Unit
 
 }
