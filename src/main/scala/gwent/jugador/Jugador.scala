7843551c25c2024d6acf31ae3a0d07d17dcaf9b4
ElVichoSiu <vthielemunoz@gmail.com>
2023-06-02 21:45:56 -0400

    REFACTOR: Changed the order for the methods in Class Jugador

--- src/main/scala/gwent/jugador/Jugador.scala
+++ src/main/scala/gwent/jugador/Jugador.scala
@@ -8,18 +8,17 @@ import cl.uchile.dcc.gwent.carta.cartaunidad.classesunidad.{ADistancia, Asedio, 
 class Jugador(val name: String, private var _deck: List[ICarta], var gems: Int = 2,
               private var _hand: List[ICarta] = List(), private var _adistanciazone: List[ADistancia] = List(),
               private var _asediozone: List[Asedio] = List(), private var _caczone: List[CuerpoaCuerpo] = List()) extends IJugador {
-  
-  def adistanciazone: List[ADistancia] = _adistanciazone
 
-  def asediozone: List[Asedio] = _asediozone
+  def shuffleDeck(): Unit = {
+    _deck = scala.util.Random.shuffle(_deck)
 
-  def caczone: List[CuerpoaCuerpo] = _caczone
+  }
 
-  def playCuerpoaCuerpo(c: CuerpoaCuerpo): Unit = {
-    if (hand.contains(c)) {
-      _hand = hand.filterNot(_ == c)
-      _caczone = c :: caczone
-    }
+  def drawCarta(): ICarta = {
+    val carta = deck.head
+    _deck = deck.tail
+    _hand = carta :: hand
+    carta
 
   }
 
@@ -39,21 +38,22 @@ class Jugador(val name: String, private var _deck: List[ICarta], var gems: Int =
 
   }
 
+  def playCuerpoaCuerpo(c: CuerpoaCuerpo): Unit = {
+    if (hand.contains(c)) {
+      _hand = hand.filterNot(_ == c)
+      _caczone = c :: caczone
+    }
+
+  }
+
   def deck: List[ICarta] = _deck
 
   def hand: List[ICarta] = _hand
 
-  def shuffleDeck(): Unit = {
-    _deck = scala.util.Random.shuffle(_deck)
+  def adistanciazone: List[ADistancia] = _adistanciazone
 
-  }
+  def asediozone: List[Asedio] = _asediozone
 
-  def drawCarta(): ICarta = {
-    val carta = deck.head
-    _deck = deck.tail
-    _hand = carta :: hand
-    carta
-
-  }
+  def caczone: List[CuerpoaCuerpo] = _caczone
 
 }
