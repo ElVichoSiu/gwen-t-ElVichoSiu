4982de3e2c87abab807f43066f9532ce60d70a4a
ElVichoSiu <vthielemunoz@gmail.com>
2023-07-19 21:40:18 -0400

    FEAT: Added methods to class TurnoJugador

--- src/main/scala/gwent/controlador/state/TurnoJugador.scala
+++ src/main/scala/gwent/controlador/state/TurnoJugador.scala
@@ -5,4 +5,11 @@ import gwent.controlador.GameController
 
 class TurnoJugador(context: GameController) extends GameState(context) {
 
+  override def jugarCarta(c: GameController): Unit = {
+    c.state = new TurnoPC(c)
+  }
+
+  override def pasar(c: GameController): Unit = {
+    c.state = new SoloTurnoPC(c)
+  }
 }
