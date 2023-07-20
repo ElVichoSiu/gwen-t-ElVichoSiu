52c86542d8144d6134ac7b019fdbd2a968570ebb
ElVichoSiu <vthielemunoz@gmail.com>
2023-07-19 21:16:59 -0400

    FEAT: Added a ovverride method for the class GameStart to have change in state of the context

--- src/main/scala/gwent/controlador/state/GameStart.scala
+++ src/main/scala/gwent/controlador/state/GameStart.scala
@@ -4,5 +4,7 @@ package gwent.controlador.state
 import gwent.controlador.GameController
 
 class GameStart(context: GameController) extends GameState(context) {
-
+  override def empieza(c: GameController): Unit = {
+    c.state = new TurnoJugador(c)
+  }
 }
