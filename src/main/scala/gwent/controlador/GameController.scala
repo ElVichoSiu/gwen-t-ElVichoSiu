12eff54f2519efb99e3699bf67e24e95fb72a46d
ElVichoSiu <vthielemunoz@gmail.com>
2023-07-19 20:58:09 -0400

    FEAT: Added new methods for the class GameController

--- src/main/scala/gwent/controlador/GameController.scala
+++ src/main/scala/gwent/controlador/GameController.scala
@@ -1,6 +1,24 @@
 package cl.uchile.dcc
 package gwent.controlador
 
+import gwent.controlador.state.{GameStart, GameState}
+import gwent.jugador.Jugador
+
+import cl.uchile.dcc.gwent.carta.ICarta
+
 class GameController {
+  private var playerCharacters = List.empty[Jugador]
+  private var enemyCharacters = List.empty[Jugador]
+
+  private var state: GameState = new GameStart(this)
+
+  def addPlayerCharacter(name: String, deck: List[ICarta]): Unit = {
+    playerCharacters = playerCharacters :+ new Jugador(name, deck)
+  }
+
+  def addEnemyCharacter(name: String, deck: List[ICarta]): Unit = {
+    enemyCharacters = enemyCharacters :+ new Jugador(name, deck)
+  }
+  
 
 }
